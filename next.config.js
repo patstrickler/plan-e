42a03d5bab519dcfcab6d4b83d037e374b58c315
Patrick Strickler <patrickstrickler@Macbook-S1.local>
2025-11-20 14:18:30 -0500

    Fix build error: Remove static export config to support API routes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  basePath: '/plan-e',
-  assetPrefix: '/plan-e',
 }
 
 module.exports = nextConfig
